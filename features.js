ed987f7534d920ca7874941398aa4ef90187085f
Rashed Arman <rashedaraman21@gmail.com>
2022-12-04 03:36:17 +0600

    Fix: image not appear in live version

--- features.js
+++ features.js
@@ -1,31 +1,31 @@
 const featureData = [
   {
     name: 'Design',
-    image: '/public/feature1.webp',
+    image: './public/feature1.webp',
     description:
       'Mockup, build and verify components in Storybook, even in complete isolation from the backend. Work on design without worrying about data.',
   },
   {
     name: 'Test',
-    image: '/public/feature2.png',
+    image: './public/feature2.png',
     description:
       "Whether it's TDD or DDT, Redwood uses Jest along with mocks and scenarios to verify functionality on the frontend and the backend.",
   },
   {
     name: 'Auth & Security',
-    image: '/public/feature3.webp',
+    image: './public/feature3.webp',
     description:
       'Lock down your front and backends with your own self-hosted auth or integrate with nearly a dozen third party auth providers. Verify access with RBAC and Validations.',
   },
   {
     name: 'Data & Transport',
-    image: '/public/feature4.webp',
+    image: './public/feature4.webp',
     description:
       "When your API is GraphQL you're ready for any client, from the browser to native mobile apps and more. Transform your data with Directives and keep your business logic organized and reusable within Services.",
   },
   {
     name: 'Deploy',
-    image: '/public/feature5.webp',
+    image: './public/feature5.webp',
     description:
       'Deploy your app to serverless hosts like Netlify and Vercel, build for containers, or deploy to bare metal.',
   },
--- technologies.js
+++ technologies.js
@@ -1,31 +1,31 @@
 const technologiesData = [
   {
     name: 'React',
-    image: '/public/react.svg',
+    image: './public/react.svg',
     description:
       'We’ve chosen the world’s most popular rendering engine to power Greywood’s web frontend. With React, you’ll have your pick of learning materials, design systems, and trained employees.',
   },
   {
     name: 'GraphQL',
-    image: '/public/graphql.svg',
+    image: './public/graphql.svg',
     description:
       'As your project grows, so will the number of client apps that need to talk to your backend. With a GraphQL API as your foundation, you can live in a multi-client world with ease.',
   },
   {
     name: 'TypeScript',
-    image: '/public/typescript.svg',
+    image: './public/typescript.svg',
     description:
       'Optional TypeScript support gives you the best of JavaScript AND the best of a type system. Even if you build your app in JS, you’ll still get killer autocomplete because Greywood itself is written in TypeScript.',
   },
   {
     name: 'Jest',
-    image: '/public/jest.svg',
+    image: './public/jest.svg',
     description:
       'Sleep well at night knowing that your critical code paths are well tested. Greywood augments Jest with “scenarios” making it easy to setup your database for a given scenario, plus GraphQL mocking to easily test data flow.',
   },
   {
     name: 'Storybook',
-    image: '/public/storybook.svg',
+    image: './public/storybook.svg',
     description:
       'If you design your components in isolation with Storybook, not only will you never have to fight with your framework to see it in a specific state, but you’ll also build up a comprehensive design reference for free!',
   },
